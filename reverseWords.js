52f81c7100a84e459fce6d4627e75a3229897299
Mona Temchin <monatemchin@gmail.com>
2019-01-14 15:10:04 -0500

    Added first version of reverseWords algorithm

---
+++ reverseWords.js
@@ -0,0 +1,18 @@
+const reverseWords = (string) => {
+  let reverseArray = [];
+  reverseArray = string.split(' ');
+  reverseWordString = '';
+  reverseArray.forEach(word => {
+     let letterArray = word.split('');
+     let reverseWord = [];
+     for (i = word.length-1; i >= 0; i--) {
+        reverseWord.push(letterArray[i]);
+     }
+  backwardsWord = reverseWord.join('');
+  reverseWordString = reverseWordString +  backwardsWord + ' ';
+  });
+  console.log(reverseWordString);
+
+};
+
+reverseWords('this is a string of words');
