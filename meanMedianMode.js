1250dd0bb9ebf649615f9cb06d3b15bc0bcf33ec
Mona Temchin <monatemchin@gmail.com>
2019-02-24 01:23:49 -0500

    Coded skeleton for meanMedianMode--3 functions getMean, getMedian, getMode and main function calls all three

---
+++ meanMedianMode.js
@@ -0,0 +1,17 @@
+const getMean = (array) => {
+
+}
+
+const getMedian = (array) => {
+
+}
+
+const getMode = (array) => {
+
+}
+
+const meanMedianMode = (array) => {
+  getMean(array);
+  getMedian(array);
+  getMode(array);
+}
