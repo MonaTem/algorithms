23249c351282766c6a7df74bbcb8076f5b703e78
Mona Temchin <monatemchin@gmail.com>
2019-03-05 23:49:12 -0500

    Developed twoSum O(n) solution and renamed to twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,18 @@
+const twoSum = (numArray, sum) => {
+  var pairs = [];
+  var hashtable = [];
+
+for (i = 0; i < numArray.length; i++)  {
+  var currNum = numArray[i];
+  var diff = sum - currNum;
+  hashtable.push(currNum);
+  if (hashtable.includes(diff)) {
+    pairs.push(currNum, diff);
+  }
+}
+return pairs;
+}
+
+var numArray = [1, 6, 4, 5, 3, 3];
+var sum = 7;
+twoSum(numArray, sum); 
