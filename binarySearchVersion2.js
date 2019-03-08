cc64fba19c3c7cce3488a50ad2b2a8b4af2ca9cf
Mona Temchin <monatemchin@gmail.com>
2019-03-07 21:08:37 -0500

    Second solution for recursive binary search algorithm

---
+++ binarySearchVersion2.js
@@ -0,0 +1,16 @@
+const binarySearch = (numArray, key) => {
+  var middleIdx = Math.floor(numArray.length / 2);
+  var middleElement = numArray[middleIdx];
+  // console.log(middleIdx, middleElement);
+  if (middleElement === key) return true;
+  else if (middleElement < key && numArray.length > 1)
+    return binarySearch(numArray.splice(middleIdx, numArray.length), key);
+  else if (middleElement > key && numArray.length > 1)
+    return binarySearch(numArray.splice(0, middleIdx), key);
+  else return false;
+}
+
+binarySearch([5, 7, 12, 36, 39, 42, 56, 71], 56);
+// binarySearch([3, 5, 7], 6);
+// binarySearch([2, 4, 6, 8, 10, 12], 12);
+// binarySearch([2, 4, 6, 8, 10, 12], 13);
