1bbfc9ca287aef4d9a4e48789b97d674fc633f0c
Mona Temchin <monatemchin@gmail.com>
2019-03-16 20:53:45 -0400

    Fixed Max Stock Profit logic so it worked

--- maxStockProfit.js
+++ maxStockProfit.js
@@ -1,22 +1,40 @@
-const sortNumbers = (a, b) => {
-  return a > b ? 1 : b > a ? -1 : 0;
-}
-
 const maxStockProfit = (pricesArr) => {
 // takes in array of prices as a parameter
 // returns the maximum possible profit of the day
 // If no profit possible, return -1
 // Profit of zero treated like any other profit
-  let maxProfitToday = 0;
-  pricesArr.sort(sortNumbers);
-  console.log(pricesArr);
-  maxProfitToday =
-      pricesArr[pricesArr.length - 1] -
-      pricesArr[0];
-  console.log(maxProfitToday);
-  if (maxProfitToday >= 0) {
-     return maxProfitToday;
-  } else return -1;
+
+let maxProfit = -1;
+let buyPrice = 0;
+let sellPrice = 0;
+// let changePrice = false;
+
+  for (i = 0; i < pricesArr.length; i++) {
+
+    if (i === 0) {
+      buyPrice = pricesArr[i];
+      sellPrice = pricesArr[i + 1];
+    }
+
+    if (pricesArr[i+1] > sellPrice) {
+      sellPrice = pricesArr[i+1];
+    }
+
+    if (pricesArr[i] < buyPrice){
+      buyPrice = pricesArr[i];
+      sellPrice = 0;
+    }
+
+  }
+
+  if (sellPrice >= buyPrice) {
+    maxProfit = sellPrice - buyPrice;
+  }
+  return maxProfit;
 }
 
-maxStockProfit([32, 46, 26, 38, 40, 48, 42]);
+
+
+// maxStockProfit([32, 46, 26, 38, 40, 48, 42]);
+// maxStockProfit([10, 18, 4, 5, 9, 6, 16, 12]);
+maxStockProfit([0, 400, 3, 600, 4, 500]);
